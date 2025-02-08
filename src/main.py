0a568b385a66394f5079a0467036f3b014339727
ajoe2 <ajoe2468@gmail.com>
2025-02-08 14:14:19 -0800

    Changed testing framework

--- src/main.py
+++ src/main.py
@@ -11,26 +11,16 @@ meet_results_path = swimdatabase_path + r"/data/meetData"
 
 def main():
     """
-    Run SwimDatabase application or test mode if -t flag is present.
+    Run SwimDatabase application.
     """
     print(Interface.LOGO)
-    args = sys.argv[1:]
-    if args != [] and args[0] == "-t":
-        run_tests()
-    else:
-        database = dp.read_cl2(meet_results_path)
-        print(Interface.FILE_LOAD_END_STR)
-        ui = Interface(database)
-        running = True
-        while running:
-            running = ui.print_menu_and_process_input()
+    database = dp.read_cl2(meet_results_path)
+    print(Interface.FILE_LOAD_END_STR)
+    ui = Interface(database)
+    running = True
+    while running:
+        running = ui.print_menu_and_process_input()
     print("Program terminated!")      
-
-def run_tests():
-    """
-    Run database application tests. Invoked at command line with -t flag.
-    """
-    print("Running tests...")
     
 if __name__ == "__main__":
     main()
